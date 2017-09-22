c13e509a00197ea04bf1724e38318b186baa8425
Amir Salameh <asalameh@internal.synopsys.com>
2017-09-22 11:24:30 +0100

    Finished week1

--- WhichPvaluesCanYouExpect.R
+++ WhichPvaluesCanYouExpect.R
@@ -6,13 +6,13 @@ options(scipen=999)
 #Set number of simulations
 nSims <- 100000 #number of simulated experiments
 
-M<-106 #Mean IQ score in the sample (will be compared with 100 in a one-sample t-test)
-n<-26 #set sample size
+M<-108 #Mean IQ score in the sample (will be compared with 100 in a one-sample t-test)
+n<-51 #set sample size
 SD<-15 #SD of the simulated data
 #With a mean difference of 6, and SD of 15, and a sample size of 26, the test has 50% power)
   
 p <-numeric(nSims) #set up empty variable to store all simulated p-values
-bars<-20
+bars<-100
 #Run simulation
 for(i in 1:nSims){ #for each simulated experiment
   x<-rnorm(n = n, mean = M, sd = SD) #Simulate data with specified mean, standard deviation, and sample size
@@ -30,7 +30,7 @@ power<-pwr.t.test(d=(M-100)/SD, n=n,sig.level=0.05,type="one.sample",alternative
 op <- par(mar = c(5,7,4,4)) #change white-space around graph
 hist(p, breaks=bars, xlab="P-values", ylab="number of p-values\n", axes=FALSE,
      main=paste("P-value Distribution with",round(power*100, digits=1),"% Power"),
-     col="grey", xlim=c(0,1),  ylim=c(0, nSims))
+     col="grey", xlim=c(0,0.05),  ylim=c(0, 10000))
 axis(side=1, at=seq(0,1, 0.1), labels=seq(0,1,0.1))
 axis(side=2, at=seq(0,nSims, nSims/4), labels=seq(0,nSims, nSims/4), las=2)
 abline(h=nSims/bars, col = "red", lty=3)
